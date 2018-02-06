9f22b741d4c69bb69e9cc8ec4e468027b04ca852
deepfry <jj@4cc.co>
2018-02-06 15:32:58 -0800

    countdown is now working

--- assets/js/zz_custom.js
+++ assets/js/zz_custom.js
@@ -6,3 +6,44 @@ $(document).on('scroll', function(){
 		$('header').removeClass('sticky');
 	}
 })
+
+
+	// Set the date we're counting down to
+	var countDownDate = ''
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+	  // Get todays date and time
+	  var now = new Date().getTime();
+
+	  // Find the distance between now an the count down date
+	  var distance = countDownDate - now;
+
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	  // Display the result in the element with id="demo"
+	  console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ")
+	  $('#countdown-timer .countdown-days').html(days)
+	  $('#countdown-timer .countdown-hours').html(hours)
+	  $('#countdown-timer .countdown-minutes').html(minutes)
+	  $('#countdown-timer .countdown-seconds').html(seconds)
+	  //document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+	  // If the count down is finished, write some text 
+	  if (distance < 0) {
+	    clearInterval(x);
+	    //document.getElementById("demo").innerHTML = "EXPIRED";
+	  }
+	}, 1000);
+
+
+$(document).ready(function(){
+	if ($('#date-container').attr('data-date') != undefined){
+		countDownDate = new Date($('#date-container').attr('data-date'))
+	}
+})
